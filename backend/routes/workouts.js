0dcb5a8109214dac274b486225d9d72cd2d65da1
Godfred <godfrednai@gmail.com>
2023-03-30 17:56:12 +0000

    modified to include controler functions

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,29 +1,26 @@
 const express = require("express");
-
 const router = express.Router();
+const {
+  createWorkout,
+  getWorkouts,
+  getWorkout,
+  deleteWorkout,
+  updateWorkout
+} = require("../controllers/workoutController");
 
 //get all workouts
-router.get("/", (req, res) => {
-  res.json({ mssg: "GET all wokouts" });
-});
+router.get("/", getWorkouts);
 
-//get a single route
-router.get("/:id", (req, res) => {
-  res.json({ mssg: "GET a single workout" });
-});
+//get a single workout
+router.get("/:id", getWorkout);
 
 //post a new workout
-router.post("/", (req, res) => {
-  res.json({ mssg: "POST a new workout " });
-});
+router.post("/", createWorkout);
 
 //delete a workout
-router.delete("/:id", (req, res) => {
-  res.json({ mssg: "DELETE a new workout " });
-});
+router.delete("/:id", deleteWorkout);
 
 //update a workout
-router.patch("/:id", (req, res) => {
-  res.json({ mssg: "UPDATE a new workout " });
-});
+router.patch("/:id", updateWorkout);
+
 module.exports = router;
