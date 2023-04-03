936f1c0f0fd2cfb5736172410e468ae78cf095bc
Godfred <godfrednai@gmail.com>
2023-04-03 19:44:16 +0000

    added global context for workouts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home"
+import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Navbar/>
+        <Navbar />
         <div className="max-w-[1100px] p-5 my-0 mx-auto">
           <Routes>
             <Route path="/" element={<Home />} />
