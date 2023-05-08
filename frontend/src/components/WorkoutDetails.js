3b5815e7bcb6223637b6cd89a9ed838863b04ff4
Godfred <godfrednai@gmail.com>
2023-05-08 13:47:05 +0000

    minor UI changes

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -7,13 +7,15 @@ const WorkoutDetails = ({ workout }) => {
   const { dispatch } = useWorkoutsContext();
 
   const handleClick = async () => {
-    const response = await fetch("/api/workouts/" + workout._id, {
-      method: "DELETE",
-    });
-    const json = await response.json();
+    if(window.confirm("Delete Workout?")){
+      const response = await fetch("/api/workouts/" + workout._id, {
+        method: "DELETE",
+      });
+      const json = await response.json();
 
-    if (response.ok) {
-      dispatch({ type: "DELETE_WORKOUT", payload: json });
+      if (response.ok) {
+        dispatch({ type: "DELETE_WORKOUT", payload: json });
+      }
     }
   };
   return (
