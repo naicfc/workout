4235ac12043904c520c903eee8caf5f0a078cde1
Godfred <godfrednai@gmail.com>
2023-05-10 12:48:11 +0000

    added user routes

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const workoutRoutes = require("./routes/workouts");
+const userRoutes = require("./routes/user");
 
 //express app
 const app = express();
@@ -16,6 +17,7 @@ app.use((req, res, next) => {
 
 //routes
 app.use("/api/workouts", workoutRoutes);
+app.use("/api/user", userRoutes);
 
 //connect to db
 mongoose
@@ -23,7 +25,10 @@ mongoose
   .then(() => {
     //listen for requests
     app.listen(process.env.PORT, () => {
-      console.log("Connected to database & listening on port", process.env.PORT);
+      console.log(
+        "Connected to database & listening on port",
+        process.env.PORT
+      );
     });
   })
   .catch((error) => {
