6fcbd1ccba5af9f9845972b87e4e80574f7ebe07
Godfred Nai <godfrednai@gmail.com>
2023-11-02 16:39:37 +0000

    added logout navigation link

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,16 +1,47 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import { useLogout } from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 const Navbar = () => {
-  return (
-    <header className='bg-white'>
-        <div className="max-w-[1100px] my-0 mx-auto p-5 flex items-center justify-between">
-            <Link to="/" className='text-gray-800'>
-                <h1 className='font-extrabold text-lg'>Workout Buddy</h1>
-            </Link>
-        </div>
-    </header>
-  )
-}
+  const { logout } = useLogout();
+  const { user } = useAuthContext();
 
-export default Navbar
+  const handleClick = () => {
+    logout();
+  };
+
+  return (
+    <header className="bg-white fixed w-full ">
+      <div className="my-0 mx-auto py-5 px-8 flex items-center justify-between">
+        <Link to="/" className="text-gray-800">
+          <h1 className="font-extrabold text-lg">Workout Buddy</h1>
+        </Link>
+        <nav className="flex gap-4">
+          {user && (
+            <div className="flex gap-3">
+              <span>{user.email}</span>
+              <button
+                onClick={handleClick}
+                className="px-3 py-1 border rounded border-emerald-500">
+                Logout
+              </button>
+            </div>
+          )}
+          {!user && (
+            <div className="flex gap-2">
+              <Link to="/login" className="px-3 py-1">
+                Login
+              </Link>
+              <Link to="/signup" className="px-3 py-1">
+                Signup
+              </Link>
+            </div>
+          )}
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
